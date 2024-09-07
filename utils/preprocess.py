8a4773bc2b58e9f221ac125540942b4d51f1ece1
felipeos <duda@duck.com>
2024-09-07 18:43:34 -0300

    new file:   notebooks/attribute_names.ipynb 	modified:   utils/preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -10,16 +10,9 @@ def preprocess_dtypes(df):
     """
 
     copy = df.copy()
-
+    
     # 1. Objects to category -----------------------------------
     copy[copy.select_dtypes('object').columns] = copy.select_dtypes('object').astype('category')
-
-    # df_bin['Target'] = df_bin['Target'].map(lambda x: x == 1)
-    # df_bin['Attribute19'] = df_bin['Attribute19'].map(lambda x: x == 'A192')
-    # df_bin['Attribute20'] = df_bin['Attribute20'].map(lambda x: x == 'A201')
-
-    # # Convert to bool dtype
-    # df_bin[['Target', 'Attribute19', 'Attribute20']] = df_bin[['Target', 'Attribute19', 'Attribute20']].astype('bool')
 
     # 2. Booleans ----------------------------------------------
 
@@ -35,3 +28,32 @@ def preprocess_dtypes(df):
     copy[['Target', 'Attribute19', 'Attribute20']] = copy[['Target', 'Attribute19', 'Attribute20']].astype('bool')
 
     return copy
+
+def set_column_names(df):
+    """
+    Sets descriptive names for every column in the German Credit
+    @df: the actual dataframe to rename columns
+    """    
+    df.columns = [
+        'Checking Acc.', 
+        'Duration (mo)',
+        'Credit History',
+        'Purpose',
+        'Credit Amount',
+        'Savings Acc.',
+        'Employment Since',
+        'Installment/Income',
+        'Marital Status',
+        'Other Debtors',
+        'Residence Since',
+        'Property',
+        'Age',
+        'Other Installment',
+        'Housing',
+        'Existing Credits',
+        'Job',
+        'Dependents',
+        'Telephone',
+        'Foreigner',
+        'Target'
+    ]
